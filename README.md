ecfd55da09c6b161f595cc9167e0046bd5734eea
robmoss2k <15984835+robmoss2k@users.noreply.github.com>
2021-03-05 12:53:18 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# ecs-cli
-AWS ECS CLI
+﻿# ![Amazon ECS CLI](https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ECS_CLI.html)
+
+The Amazon ECS Command Line Interface (CLI) is a command line tool for Amazon Elastic Container
+Service (Amazon ECS) that provides high-level commands to simplify creating, updating, and
+monitoring clusters and tasks from a local development environment.
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://amazon-ecs-cli.s3.amazonaws.com/ecs-cli-windows-amd64-latest.exe'
+$checksum = "062F6E642F80A3073B8FAFA3F0A865C29FBC31C09BB71C0A8F18C8427DA434C4"
+$packageArgs = @{
+  packageName  = $env:ChocolateyPackageName
+  Url          = $url
+  Checksum     = $checksum
+  checksumType = 'sha256'
+  FileFullPath = Join-Path $toolsDir "$($env:ChocolateyPackageName).exe"
+}
+Get-ChocolateyWebFile @packageArgs
